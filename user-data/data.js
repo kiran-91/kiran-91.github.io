a02801fc3f580134c2dbf58bf87918a0aa47a389
kiran-91 <deshpandekiran007kd@gmail.com>
2025-01-31 13:45:21 +0530

    enabled dark mode

--- user-data/data.js
+++ user-data/data.js
@@ -71,18 +71,19 @@ export const footer = [
   //       },
   //     ],
   //   },
-  //   {
-  //     label: "Resources",
-  //     data: [
-  //       {
-  //         text: "Enable Dark/Light Mode",
-  //         func: "enableDarkMode()",
-  //       },
-  //       {
-  //         text: "Print this page",
-  //         func: "window.print()",
-  //       },
-  //       {
+     {
+       label: "Resources",
+       data: [
+         {
+           text: "Enable Dark/Light Mode",
+           func: "enableDarkMode()",
+         },
+         {
+           text: "Print this page",
+           func: "window.print()",
+         },
+        ],
+        },
   //         text: "Clone this page",
   //         link: "https://github.com/",
   //       },
