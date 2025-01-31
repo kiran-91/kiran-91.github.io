b7967e98409d7179d3c2e4a6b754a458bbe00a26
kiran-91 <deshpandekiran007kd@gmail.com>
2025-01-31 12:53:47 +0530

    adding files

---
+++ user-data/data.js
@@ -0,0 +1,115 @@
+export const bio = [
+  "Hello, I am Kiran Deshpande!",
+  "<strong>Data Scientist</strong> I've always been curious about how things work, and that's what drew me to data science.",
+  "I love the idea of using data to tell stories and answer questions, and I've been lucky enough to work on some really interesting projects. I'm always looking for new ways to apply these skills to real-world problems",
+  "Skilled in Machine Learning, Deep Learning, NLP, GenAI. I have a strong foundation in statistics and programming.",
+  "Passionate about mentoring, teamwork, teaching, and continuous improvement in engineering processes.",
+];
+
+export const skills = [
+  "Python, Data Analysis, Machine Learning, Deep Learning, NLP, GenAI",
+  "HuggingFace, Langchain",
+  "GitHub, Streamlit, Git, Docker",
+];
+
+export const experience = [
+  {
+    title: "MyShubhLife",
+    duration: "August 2024 - Present",
+    subtitle: "Data Scientist (Intern)",
+    details: [
+      "Designed an automated ML web app/GUI with a user-friendly, no-code interface, enabling end-to-end processes like statistical analysis, EDA, data preprocessing, and ML model building with results.",
+      "Analysed CIBIL reports to uncover insights and patterns for credit evaluation and risk assessment.",
+      "Conducted in-depth analysis of client data to identify correlations and associations between variables, providing actionable insights for the company's strategic planning.",
+    ],
+    tags: ["Data Analysis", "Data Visualization", "Machine Learning", "Statistical Modelling"],
+    icon: "truck ",
+  },
+];
+
+export const education = [
+  {
+    title: "M.Sc. in Data Science",
+    duration: "2022 - 2024",
+    subtitle: "Vellore Institute of Technology, Vellore",
+    details: [],
+    tags: [
+      "Statistics/Mathematics",
+      "Machine Learning",
+      "Deep Learning",
+      "Artificial Intelligence",
+      "Python",
+    ],
+    icon: "graduation-cap",
+  },
+  {
+    title: "B.Sc. in Electronics and Communication Technology",
+    duration: "2019 - 2022",
+    subtitle: "Loyola Academy, Hyderabad",
+    details: [],
+    tags: ["Mathematics", "MicroProcessor & Controller", "Internet of Things"],
+    icon: "book",
+  },
+];
+
+export const footer = [
+  //   {
+  //     label: "Dev Profiles",
+  //     data: [
+  //       {
+  //         text: "Stackoverflow",
+  //         link: "https://stackoverflow.com",
+  //       },
+  //       {
+  //         text: "GitHub",
+  //         link: "https://github.com/kiran-91",
+  //       },
+  //       {
+  //         text: "LeetCode",
+  //         link: "https://leetcode.com",
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     label: "Resources",
+  //     data: [
+  //       {
+  //         text: "Enable Dark/Light Mode",
+  //         func: "enableDarkMode()",
+  //       },
+  //       {
+  //         text: "Print this page",
+  //         func: "window.print()",
+  //       },
+  //       {
+  //         text: "Clone this page",
+  //         link: "https://github.com/",
+  //       },
+  //     ],
+  //   },
+  {
+    label: "Links",
+    data: [
+         {
+           text: "Linkedin",
+           link: "https://www.linkedin.com/in/kiran-deshpande-7173ba211/",
+         },
+      //   {
+      //     text: "Twitter",
+      //     link: "https://twitter.com/thesigmakid",
+      //   },
+      //{
+      //  text: "Buy me a coffee",
+      //  link: "https://www.buymeacoffee.com/r194dME8y",
+      //},
+      {
+        text: "Github",
+        link: "https://github.com/kiran-91",
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: ["Made with &hearts; by Kiran Deshpande"],
+  },
+];
